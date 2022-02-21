c44471dd2a14271c627d95b127edaa76dd0b9d57
Afvanie <ardyansah201@gmail.com>
2022-02-21 20:24:10 +0700

    ganti controller single

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,25 @@
 <?php
 
-USE App\Http\Controllers\PageController;
+use Illuminate\Support\Facades\Route;
 
-Route::get('/',[PageController::class, 'index']);
-Route::get('/about',[PageController::class, 'about']);
-Route::get('/articles/{id}',[PageController::class, 'articles']);
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+/*Route::get('/', function () {
+    return view('welcome');
+});*/
+USE App\Http\Controllers\HomeController;
+USE App\Http\Controllers\AboutController;
+USE App\Http\Controllers\ArticleController;
+
+Route::get('/',[HomeController::class, 'index']);
+Route::get('/about',[AboutController::class, 'about']);
+Route::get('/articles/{id}',[ArticleController::class, 'articles']);
